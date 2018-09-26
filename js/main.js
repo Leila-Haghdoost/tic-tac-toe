e9389e59a8ea04484ce4b527f47410d5419220ae
leila-Haghdoost <haghdoost62@gmail.com>
2018-09-26 16:23:54 +1000

    tic-tac-toe second edition

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-
 let player = "X";
 let move = 1; // counter of clicks
 let gameOver = false;
 
-const board = [
+let board = [
   null, null, null,
   null, null, null,
   null, null, null
 ]; /// It takes the value of each cell to use in if statement
+
 
 const winPatterns = function(player) {
 
@@ -22,6 +22,7 @@ const winPatterns = function(player) {
     board[0] === player && board[4] === player && board[8] === player
   ) {
     gameOver = true;
+    // console.log(gameOver);
     return player;
   } else {
     return `Game Over!`;
@@ -29,16 +30,32 @@ const winPatterns = function(player) {
 };
 
 
-const resetGame = function() {
-  console.log('reset!');
-  if (gameOver) {
 
-    return false;
+const trigger = function(player) {
+  console.log('trigger()');
+  $(".popup-content").text(`Player ${player} won the game.`)
+  // $(".popup-overlay").hide();
+  $(".popup-overlay").show();
+};
+
+const resetGame = function() {
+  // console.log('reset!');
+  // console.log(gameOver);
+  if (gameOver) {
+    board = [
+      null, null, null,
+      null, null, null,
+      null, null, null
+    ];
+    // console.log(board);
+    move = 1;
+    player = "X";
+    gameOver = false;
+    // return gameOver = false;
   }
   // reset
 
 }
-
 
 const playMove = function(clickedSquare) {
   // let id = $("table tr td").attr("id");
@@ -66,31 +83,40 @@ const playMove = function(clickedSquare) {
   move++; //it counts next moves.
 };
 
-
-
 //JQUERY/////////////////////////////
 
 $("table tr td").on("click", function() {
   // console.log($(this).attr("id"));
-  let id = $(this).attr("id");//it takes the id of each cell.
-  let $text = $(this).text();
-  // console.log($text);
+  let id = $(this).attr("id"); //it takes the id of each cell.
+  let $text = $(this).html();
   if (move <= 9) { //max of moves
 
     // check that the square is empty and check game still continues.
+    console.log('$text', `(${$text})`);
     if ($text === "" && gameOver === false) {
 
       playMove(this); //it refferes to the function which set the "x" and "o" images and css.
       board[id] = player;
       console.log(board);
 
-      if (winPatterns(player) !== '') { // it checkes the cell not to be empty.
-        if (winPatterns(player) === 'X') { //it compares the char and choose the winner.
-          alert(`player X is the winner.`);
-        } else if ((winPatterns(player) === 'O')) {
-          alert(`player O is the winner.`);
+      const winner = winPatterns(player);
+      if (winner !== '') { // it checkes the cell not to be empty.
+        if (winner === 'X') { //it compares the char and choose the winner.
+
+          // console.log(`player X is the winner.`);
+          // console.log(player);
+          trigger(player);
+        } else if ((winner === 'O')) {
+          // console.log(`* player O is the winner *`);
+          // console.log(player);
+          trigger(player);
         }
       }
+
+      // playComputerMove();
+
+      // switch back to human player
+
     } else {
       // the square is not empty - do not let the move happen
       console.log("invalid click");
@@ -102,44 +128,14 @@ $("table tr td").on("click", function() {
 
 });
 
-// $("#reset").click(resetGame);
-// $("#reset").on("click", function() {
-//   gameOver = false;
-//
-//   $("td").css({
-//     backgroundImage: "",})
-//   $("td").html("");
-//
-//   // $("table tr td").empty();
-//   player = "X";
-//   move = 1;
-// })
+$("#reset").on("click", function() {
+  $(".popup-overlay").hide();
+  resetGame();
 
+  $("td").css({
+    backgroundImage: "",
+  })
+  $("td").html("");
+  // reset the game
 
-
-
-$(document).ready(function(){
-    $("#reset").hover(function(){
-        $(this).css("background-color", "#375bcf");
-        }, function(){
-        $(this).css("background-color", "#45cb25");
-    });
 });
-$(document).ready(function(){
-    $("#start").hover(function(){
-        $(this).css("background-color", "#375bcf");
-        }, function(){
-        $(this).css("background-color", "#45cb25");
-    });
-});
-//
-
-
-
-
-
-// $("table tr td").ready(function(){
-//     $("#reset").click(function(){
-//         $("td").css({ backgroundImage:""});
-//     });
-// });
